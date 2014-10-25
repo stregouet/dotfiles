407f55103d88a760f9aabac888d19c65b4e26dbd
Samuel TRÉGOUËT <samuel.tregouet@gmail.com>
2014-10-25 23:24:53 +0200

    [emacs] configure multiple cursors plugin

--- emacs/init-packages/init-multiple-cursors.el
+++ emacs/init-packages/init-multiple-cursors.el
@@ -1,2 +1,9 @@
+(require 'multiple-cursors)
 (global-set-key (kbd "C-c c") 'mc/edit-lines) ;; 1 cursor/line d'une region
-(global-set-key (kbd "C-'") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-%") 'mc/mark-more-like-this-extended)
+
+(setq mc/mark-more-like-this-extended-keymap (make-sparse-keymap))
+(define-key mc/mark-more-like-this-extended-keymap (kbd "C-r") 'mc/mmlte--up)
+(define-key mc/mark-more-like-this-extended-keymap (kbd "C-s") 'mc/mmlte--down)
+(define-key mc/mark-more-like-this-extended-keymap (kbd "C-t") 'mc/mmlte--left)
+(define-key mc/mark-more-like-this-extended-keymap (kbd "C-n") 'mc/mmlte--right)
