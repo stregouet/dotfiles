ae1fa90585b0a7fb6737fdb81ac115667dca6d80
Samuel TRÉGOUËT <samuel.tregouet@gmail.com>
2015-05-05 19:17:51 +0200

    [emacs] remove el-get and use elpa with melpa instead
    add a function to load every init-*.el file in init-packages directory to
    replace el-get functionality

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,19 @@
 (add-to-list 'load-path "~/.dotfiles/emacs")
 (add-to-list 'load-path "~/.emacs.d/specific")
 
+
+(defun my-load-init-packages (dir-name)
+  "list directory `dir-name' and apply `load' on each el file"
+  (add-to-list 'load-path dir-name)
+  (let ((list-abs-el-file (directory-files dir-name nil "el$")))
+    (mapcar 'load
+            (mapcar 'file-name-base list-abs-el-file))))
+
+
 (require 'my-keybindings)
-(require 'setup-el-get)
+(require 'setup-packages)
 (require 'my-settings)
 (require 'my-hl-keywords)
+
+(my-load-init-packages "~/.dotfiles/emacs/init-packages")
+
---
+++ emacs/setup-packages.el
@@ -0,0 +1,30 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+
+(defun install-if-not-installed (package-name)
+  (when (not (package-installed-p package-name))
+    (package-install package-name)))
+
+(mapcar 'install-if-not-installed
+        '(color-theme-solarized
+          rust-mode
+          stylus-mode
+          expand-region
+          multiple-cursors
+          emmet-mode
+          coffee-mode
+          flx-ido
+          hlinum
+          web-beautify
+          flycheck
+          fill-column-indicator
+          undo-tree
+          smartparens
+          ))
+
+
+(provide 'setup-packages)
