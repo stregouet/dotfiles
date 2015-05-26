8883f822f9e15e42856c212c963955b78c1debd4
Samuel TRÉGOUËT <samuel.tregouet@gmail.com>
2015-05-26 21:35:57 +0200

    [emacs] add git-commit-mode package

--- emacs/setup-packages.el
+++ emacs/setup-packages.el
@@ -14,6 +14,7 @@
 (mapcar 'install-if-not-installed
         '(color-theme-solarized
           org-plus-contrib
+          git-commit-mode
           systemd
           rust-mode
           stylus-mode
